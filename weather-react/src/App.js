0f3a99c50a84ca0432cc3238621e3cb409e16de1
vikchvk <vchvykova@gmail.com>
2022-11-26 11:44:59 +0200

    added footer

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,25 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Search from "./Search";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div>
-        <h1>Weather App</h1>
-        <Search />
-        <small class="coder">
-            <a
-              href="https://github.com/vikchvk/new-weather-app"
-              target="_blank"
-              rel="noreferrer"
-            >
+     <Weather defaultCity="New York" />
+
+<footer>
+  <a href="https://github.com/vikchvk/new-weather-app" target="_blank" rel="noreferrer">
               Open-source code
             </a>
             by Viktoriia Chvykova
-          </small>
-      </div>
+ </footer>
     </div>
   );
 }
